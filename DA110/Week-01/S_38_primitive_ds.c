f6f98ad7b04e118c8eb68eacd577c0a84febde2d
saurabhdbz <70940589+saurabhdbz@users.noreply.github.com>
2025-06-13 21:20:22 +0530

    Create S_38_primitive_ds.c

---
+++ DA110/Week-01/S_38_primitive_ds.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3, b = 10, c = 4, d = 6, e = 7;
+    int ele = 4;
+
+    if (a == ele)
+        printf("found\n");
+    else if (b == ele)
+        printf("found\n");
+    else if (c == ele)
+        printf("found\n");
+    else if (d == ele)
+        printf("found\n");
+    else if (e == ele)
+        printf("found\n");
+    else
+        printf("not found\n");
+
+    return 0;
+}
