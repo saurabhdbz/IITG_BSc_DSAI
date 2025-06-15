aabc9e7fd0d012647188300b21936402be54bf82
saurabhdbz <70940589+saurabhdbz@users.noreply.github.com>
2025-06-15 09:34:21 +0530

    Create S_6_Factorial.c

---
+++ DA110/Space_Complexity/S_6_Factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function prototype
+int factorial(int);
+
+int main() {
+    int n = 6;
+    int fac;
+
+    fac = factorial(n);
+
+    printf("The factorial of %d is %d\n", n, fac);
+
+    return 0;
+}
+
+// Recursive factorial function
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;  // Base case
+    } else {
+        return n * factorial(n - 1);  // Recursive case
+    }
+}
